4a89626b4f10275cb662ea78f12df30c7d993512
Rabbany861 <rabbanyislam861@gmail.com>
2024-11-23 16:50:04 +0600

    text file made by me.

---
+++ tree traversal .c
@@ -0,0 +1,56 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node*left;
+    struct Node*right;
+};
+struct Node*createNode(int value)
+{
+    struct Node*newNode=(struct Node*)malloc(sizeof(struct Node));
+    newNode->data=value;
+    newNode->left=NULL;
+    newNode->right=NULL;
+    return newNode;
+};
+void inOrder(struct Node*root)
+{
+    if(root==NULL) return;
+    inOrder(root->left);
+    printf("%d ",root->data);
+    inOrder(root->right);
+
+}
+void preOrder(struct Node*root)
+{
+    if(root==NULL) return;
+    printf("%d ",root->data);
+    preOrder(root->left);
+    preOrder(root->right);
+
+}
+void postOrder(struct Node*root)
+{
+    if(root==NULL) return;
+    postOrder(root->left);
+    postOrder(root->right);
+    printf("%d ",root->data);
+
+}
+int main()
+{
+    struct Node*root=createNode(1);
+    root->left=createNode(2);
+    root->right=createNode(3);
+    root->left->left=createNode(4);
+    root->left->right=createNode(5);
+    inOrder(root);
+    printf("\n");
+    preOrder(root);
+    printf("\n");
+
+    postOrder(root);
+    printf("\n");
+}
